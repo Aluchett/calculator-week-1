b26a8ea48630f6081905e9a3ace7ec0edbf2d4fd
Aluchett <165130853+Aluchett@users.noreply.github.com>
2024-04-08 11:06:22 +1200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Transactions;
+
+namespace calculator2week1
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your First Number: ");
+            double firstN
+           umber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Your Second Number: ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Your Operator (+, -, /, *: ");
+            string myOperator = Console.ReadLine();
+            if (myOperator == "+")
+            {
+                Console.WriteLine(firstNumber + secondNumber);
+            }
+             if (myOperator == "-")
+            {
+                Console.WriteLine(firstNumber - secondNumber);
+            }
+            if (myOperator == "/")
+            {
+                Console.WriteLine(firstNumber / secondNumber);
+                if (myOperator == "*").
+                        }
+            {
+                Console.WriteLine(firstNumber * secondNumber);
+            }
+            if (myOperator == "*")
+            {
+
+
+            }
+                Console.ReadLine();
+            
+            
+        }
+    }
+}
